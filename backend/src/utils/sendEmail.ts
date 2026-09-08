beb04bdb407d6c90dfa6675f7774a7c411267f1d
Azman988 <azeezusman571@gmail.com>
2026-09-08 15:18:30 +0100

    fix: Update email transport port to 465 and set secure to true

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 export const sendEmail = async (options: { email: string; subject: string; message: string }) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST || 'smtp.gmail.com' || 'gmail.com',
-        port: Number(process.env.EMAIL_PORT) || 587,
-        secure: false, 
+        port: Number(process.env.EMAIL_PORT) || 465,
+        secure: true, 
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
